2743221e9176dd0a1642a77a3673d958fa95bb72
kvk-code <75604208+kvk-code@users.noreply.github.com>
2023-04-17 12:36:53 +0530

    question1.c created

---
+++ starter_files/question1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+void generate_sha256_hash(const char *message, unsigned char *hash) {
+    // TODO: Implement the function
+}
+
+int main() {
+    // TODO: Test your implementation
+    return 0;
+}
